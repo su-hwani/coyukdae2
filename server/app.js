48aa4724b94a0fd9be7fe3003b20ca5218c40a81
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-01-23 17:09:29 +0900

    Feat: Ngrok deploy success!

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ console.log('Secret Key:', secretKey);
 //   },
 // };
 
-// ngrok 으로 localhost 외부 접속 허용 성공!! 
+// ngrok 으로 localhost 외부 접속 허용 성공!!! 
 const ngrokUrl = 'https://8e51-121-130-212-39.ngrok-free.app'
 
 app.use(cors({
